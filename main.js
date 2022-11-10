616cb108c193c34182e58a5e6de757ed4f3d998e
RamaniShankar1 <ramshankarjan1222@gmail.com>
2022-11-10 21:39:40 +0530

    Completed

---
+++ main.js
@@ -0,0 +1,14 @@
+var names = ['Ramani','Raja','Ajay','Sakthi','MariSelvan','Muthu','Sabaree','Sudesan','Vallinayagam']
+var rName=names.filter(name=>name.startsWith('R'))
+console.log(rName);
+var rName=names.filter(name=>name.startsWith('Sa'))
+console.log(rName);
+var rName=names.filter(name=>name.endsWith('i'))
+console.log(rName);
+var rName=names.filter(name=>name.startsWith('M'))
+console.log(rName);
+var rName=names.filter(name=>name.match('y'))
+console.log(rName);
+var rName=names.filter(name=>name.startsWith('Su'))
+console.log(rName);
+
